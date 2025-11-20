9d55cb09fb75eecf79e1638948f543764bec0aa2
westSun <taiyo6060@gmail.com>
2025-11-20 12:01:11 +0000

    WARNING：For better performance, reliability, and access to latest features, consider using Gemini directlyに従いgeminiは直接使用する
    This reverts commit cf2162de267d21fe6c85c13195124d7dd42afc22.

--- my_agent/agent.py
+++ my_agent/agent.py
@@ -1,14 +1,10 @@
 from google.adk.agents.llm_agent import Agent
-from google.adk.models.lite_llm import LiteLlm
 
 from my_agent.tools import get_current_time, get_weather
 
-PROVIDER = "vertex_ai"
-MODEL = "gemini-2.0-flash"
-
 root_agent = Agent(
     name="weather_agent_v1",
-    model=LiteLlm(model=f"{PROVIDER}/{MODEL}"),
+    model="gemini-2.0-flash",
     description="Provides weather information for specific cities.",
     instruction="You are a helpful weather assistant. "
     "When the user asks for the weather in a specific city, "
